3db02b1911ce023e116d8949a9c365a820c640a3
gsanthoshvarma <santhoshvarma.g@gmail.com>
2019-01-09 02:03:07 +0530

    Added home welcome images

--- src/test/java/com/sr/emstore/controller/AdminControllerTest.java
+++ src/test/java/com/sr/emstore/controller/AdminControllerTest.java
@@ -1,6 +1,22 @@
 package com.sr.emstore.controller;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-public class AdmincontrollerTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations= {"/WEB-INF/applicationContext.xml"})
+public class AdminControllerTest {
+	
+	@Autowired
+	private SimpleDriverDataSource dataSource;
+	
+	@Test
+	public void sampleTest() {
+		System.out.println(dataSource);
+	}
 
 }
